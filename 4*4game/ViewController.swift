218cb70976f7209a74c19535161a470803488238
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2020-10-17 20:15:38 +0900

    Initial Commit

---
+++ 4*4game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4*4game
+//
+//  Created by user on 2020/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
